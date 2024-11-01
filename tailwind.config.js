8bc7527d518f72ffb01e062edc7e097a8643a69e
Nikkihosseini <niekal00@gmail.com>
2024-11-01 13:29:33 +0330

    add styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,37 @@ export const theme = {
     fontFamily: {
       "RobotoBold": "Roboto Bold",
     },
+    colors: {
+        "lightBlue":{
+          100 : "#daf0fe",
+          150 : "#a3c3dc",
+          200 : '#edffff',
+          250 : '#c5e2f4',
+          300 : '#9cccf2', 
+          350 : '#548dba',
+          400 : '#1aacff',
+          450 : '#98f6ff',
+          500 : '#408dc5',
+          550 : '#263842',
+        },
+        "darkBlue" : {
+          100 : '#feffff',
+          150 : '#0f1f2c',
+          200 : '#192938',
+          250 : '#4197ca',
+          300 : '#273649',
+          350 : '#005eb5',
+          400 : '#44a8ff',
+          450 : '#238ef8',
+          500 : '#8dd4ff',
+        },
+        'blue': {
+          150 : '#5982a0'
+        },
+        'gray': {
+          150 : '#788895'
+        }
+    },
     container: {
       center: true,
       padding: {
